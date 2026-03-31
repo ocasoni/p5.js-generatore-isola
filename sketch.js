5f9a26462f9b53bb3e06a381e048cb184d6402ef
nik <n.somma@ied.edu>
2026-03-31 13:43:35 +0200

    funzione compute altitude

--- sketch.js
+++ sketch.js
@@ -30,19 +30,7 @@ function setup() {
  //TILES 
   for (let x = 0; x < width; x= x +TILE_SIZE) {
     for (let y = 0; y < height; y= y +TILE_SIZE) {
-      //Calcola distanza dal centro 
-      let distanceFromCenter = dist(centralX, centralY, x, y);
-      let normDistanceFromCenter = distanceFromCenter / (width / 2);
-      
-      //Calcola altitudine in base alla distanza dal centro
-      let altitude = 1-normDistanceFromCenter; // Più vicino al centro, più alto
-      
-      //Perlin
-      noiseDetail(6); // Imposta dettagli e persistenza del rumore Perlin
-      let Perlin = noise(x/PERLIN_SCALE, y/PERLIN_SCALE);
-      altitude *= Perlin;
-      altitude += Perlin;
-      altitude -= 0.5;
+      let altitude = computeAltitude(x, y, centralX, centralY);
       
       //Calcola colore
       let seaLevel = 0.2; // Livello del mare al 50% dell'altitudine massima
@@ -60,29 +48,13 @@ function setup() {
       
       image(img, x, y, TILE_SIZE, TILE_SIZE);
 
-      
-      
-      
-
     }
   }
  
 //SPRITES
   for (let x = 0; x < width; x= x +TILE_SIZE) {
     for (let y = 0; y < height; y= y +TILE_SIZE) {
-      //Calcola distanza dal centro 
-      let distanceFromCenter = dist(centralX, centralY, x, y);
-      let normDistanceFromCenter = distanceFromCenter / (width / 2);
-      
-      //Calcola altitudine in base alla distanza dal centro
-      let altitude = 1-normDistanceFromCenter; // Più vicino al centro, più alto
-      
-      //Perlin
-      noiseDetail(6); // Imposta dettagli e persistenza del rumore Perlin
-      let Perlin = noise(x/PERLIN_SCALE, y/PERLIN_SCALE);
-      altitude *= Perlin;
-      altitude += Perlin;
-      altitude -= 0.5;
+      let altitude = computeAltitude(x, y, centralX, centralY);
       
       //Calcola colore
       let seaLevel = 0.2; // Livello del mare al 50% dell'altitudine massima
@@ -103,3 +75,21 @@ function setup() {
     }
   }
 }
+function computeAltitude(x, y, centralX, centralY) {
+  //Calcola distanza dal centro 
+  let distanceFromCenter = dist(centralX, centralY, x, y);
+  let normDistanceFromCenter = distanceFromCenter / (width / 2);
+      
+  //Calcola altitudine in base alla distanza dal centro
+  let altitude = 1-normDistanceFromCenter; // Più vicino al centro, più alto
+      
+  //Perlin
+  noiseDetail(6); // Imposta dettagli e persistenza del rumore Perlin
+  let Perlin = noise(x/PERLIN_SCALE, y/PERLIN_SCALE);
+  altitude *= Perlin;
+  altitude += Perlin;
+  altitude -= 0.5;
+
+  return altitude;
+}
+
